cc0bfb871ad014197090fafa738aaef5066db7c0
Franklin <a2020115522@estudiantes.upsa.edu.bo>
2021-10-13 16:42:06 -0400

    RandomController añadido

---
+++ PracticaCancionesAPI/PracticaCancionesAPI/Controllers/RandomController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PracticaCancionesAPI.Data;
+using PracticaCancionesAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticaCancionesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public RandomController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Cancion>> GetCancion()
+        {
+
+            var list = await _context.Cancion.ToListAsync();
+
+            var max = list.Count;
+            int index = new Random().Next(0, max);
+
+            var cancion = list[index];
+
+            if (cancion == null)
+            {
+                return NoContent();
+            }
+
+            return cancion;
+        }
+
+    }
+}
